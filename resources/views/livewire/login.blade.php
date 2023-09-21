dedd5d068a6c2d05c32e439e7d94e30d0eca1822
NewbieCed <jctdelacarcel@outlook.com>
2023-09-21 20:42:32 +0800

    /design/login
    added login page
    responsive
    tailwind config updates:
    - set theme for the web app
    - set screen breakpoints

---
+++ resources/views/livewire/login.blade.php
@@ -0,0 +1,63 @@
+<div class="h-screen landing bg-center bg-no-repeat">
+    <div class="md:absolute inset-0 bg-primaryBg bg-opacity-95"></div>
+
+    <div class="w-full h-screen flex justify-center items-center">
+        <div
+            class="relative bg-white w-full h-screen px-10 py-36 md:h-fit md:z-30 md:w-96 md:p-8 md:rounded-lg md:shadow">
+            <div class="flex flex-col w-full gap-y-5 items-center ">
+                <div class="p-4 pb-0 w-36">
+                    <img src="/images/Aria.png" alt="logo" class="bg-contain bg-center" />
+                </div>
+
+                <div class="text-center text-3xl font-medium w-full">Aria HR Suite</div>
+                <div class="text-center text-2xl font-medium w-full my-4">Login</div>
+                <form class="w-full">
+                    <div class="mb-4">
+                        <label for="email-address-icon" class="block mb-2 text-sm font-medium text-gray-900">Email
+                            Address</label>
+                        <div class="relative">
+                            <div class="absolute inset-y-0 left-0 flex items-center pl-3.5 pointer-events-none">
+                                <svg class="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                    fill="currentColor" viewBox="0 0 20 16">
+                                    <path
+                                        d="m10.036 8.278 9.258-7.79A1.979 1.979 0 0 0 18 0H2A1.987 1.987 0 0 0 .641.541l9.395 7.737Z" />
+                                    <path
+                                        d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z" />
+                                </svg>
+                            </div>
+                            <input type="email" id="email-address-icon"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5"
+                                placeholder="Email address">
+                        </div>
+                    </div>
+                    <div class="mb-4 md:mb-2">
+                        <label for="email-address-icon"
+                            class="block mb-2 text-sm font-medium text-gray-900">Password</label>
+                        <div class="relative">
+                            <div class="absolute inset-y-0 left-0 flex items-center pl-3.5 pointer-events-none">
+                                <svg class="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                    fill="currentColor" viewBox="0 0 20 16">
+                                    <path
+                                        d="M14 7h-1.5V4.5a4.5 4.5 0 1 0-9 0V7H2a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2Zm-5 8a1 1 0 1 1-2 0v-3a1 1 0 1 1 2 0v3Zm1.5-8h-5V4.5a2.5 2.5 0 1 1 5 0V7Z" />
+                                </svg>
+                            </div>
+                            <input type="password" id="password-icon"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5"
+                                placeholder="Password">
+                        </div>
+                    </div>
+                    <div class="mb-6 md:mb-10 w-full">
+                        <a href="#">
+                            <p class="text-xs font-medium text-end text-primary">Forgot Password</p>
+                        </a>
+                    </div>
+                    <div>
+                        <button class="bg-primary flex flex-col justify-center w-full h-12 items-center rounded-lg">
+                            <span class="text-center font-medium text-secondary">Login</span>
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,22 @@ export default {
     ],
     theme: {
         extend: {},
+        colors: {
+            primary: "#283f7e",
+            secondary: "#DEA35A",
+            primaryBg: "#131d37",
+            // ...
+        },
+        screens: {
+            md: "640px",
+            // => @media (min-width: 640px) { ... }
+
+            lg: "1024px",
+            // => @media (min-width: 1024px) { ... }
+
+            xlg: "1280px",
+            // => @media (min-width: 1280px) { ... }
+        },
     },
     plugins: [require("flowbite/plugin")],
 };
